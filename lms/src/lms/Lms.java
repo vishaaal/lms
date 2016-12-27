e986f1c491732df6a71a4a79dd7670de8597fe26
vishaaal <vishalpancholi02@gmail.com>
2016-12-27 13:49:50 +0530

    day 1

---
+++ lms/src/lms/Lms.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lms;
+
+
+public class Lms {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        lms.front.Log_in ob = new lms.front.Log_in();
+        ob.setVisible(true);
+    }
+    
+}
